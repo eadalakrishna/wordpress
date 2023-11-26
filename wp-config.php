b6ecf8bfbef5c8267f7dda53064ac6d31695fa62
eadalakrishna <141901192+eadalakrishna@users.noreply.github.com>
2023-11-26 11:22:34 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'admin' );
 define( 'DB_PASSWORD', 'Admin123' );
 
 /** Database hostname */
-define( 'DB_HOST', 'mydatabase.cttkdmfxmxms.us-east-1.rds.amazonaws.com' );
+define( 'DB_HOST', 'database-1.cttkdmfxmxms.us-east-1.rds.amazonaws.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
